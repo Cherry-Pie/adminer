e6d168835af456fe5f9f90ac38d052f4d3172a47
Yaro <12fcv4@gmail.com>
2015-02-16 14:29:25 -0500

    remove tabs

--- src/Yaro/Adminer/AdminerServiceProvider.php
+++ src/Yaro/Adminer/AdminerServiceProvider.php
@@ -7,45 +7,45 @@ use Illuminate\Support\ServiceProvider;
 
 class AdminerServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('yaro/adminer');
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('yaro/adminer');
         
         include __DIR__.'/../../routes.php';
         
         \View::addNamespace('adminer', __DIR__.'/../../views/');
-	}
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
